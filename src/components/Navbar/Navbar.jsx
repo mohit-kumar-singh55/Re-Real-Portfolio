286ab06e64e904d2cc50f153030b7027d0ee447d
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-25 19:02:37 +0530

    creating navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import "./Navbar.scss";
+import images from "../../constants/index";
 
 const Navbar = () => {
     return (
-        <nav>
-            <div>
-                <img src={} alt="LOGO" />
+        <nav className="app__navbar">
+            <div className='app__navbar-logo'>
+                <img src={images.logo} alt="LOGO" />
             </div>
+            <ul className='app__navabr-links'>
+                {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item) => (
+                    <li key={item} className="app__flex p-text">
+                        <div />
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                ))}
+            </ul>
         </nav>
     )
 }
