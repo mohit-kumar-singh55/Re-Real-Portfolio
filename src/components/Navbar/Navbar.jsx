216c7ac4f096eaa6f66e1124e11ebf3c8277174d
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-25 18:46:50 +0530

    created assets

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,11 @@ import "./Navbar.scss";
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav>
+            <div>
+                <img src={} alt="LOGO" />
+            </div>
+        </nav>
     )
 }
 
